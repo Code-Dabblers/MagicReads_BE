b9c86371b9e41a174bdb2b5c05ebddc209b4737f
fabcodingzest <fabrizvi786@gmail.com>
2020-10-08 15:34:08 +0530

    Added few dependencies and scripts

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
-// https://medium.com/code-oil/webpack-javascript-bundling-for-both-front-end-and-back-end-b95f1b429810
+// Includes
+const path = require("path");
+const express = require("express");
+const dotenv = require("dotenv");
+
+dotenv.config({ path: "./config/config.env" });
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+// Server Listening
+app.listen(
+  PORT,
+  console.log(
+    `MagicReads is running in ${process.env.NODE_ENV} mode on server ${PORT}`
+  )
+);
