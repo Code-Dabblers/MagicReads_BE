85268bf34be10a3b536d391e57967405b30a1791
fabcodingzest <fabrizvi786@gmail.com>
2020-10-17 18:16:44 +0530

    changes in story model for summary and visibility

--- models/Story.js
+++ models/Story.js
@@ -3,20 +3,21 @@ let { customAlphabet } = require("nanoid");
 const nanoid = customAlphabet("1234567890abcdef", 10);
 
 const storySchema = new mongoose.Schema({
-  _id: { type: String, default: () => nanoid() },
-  storyName: { type: String, required: true },
-  tags: [{ type: String }],
-  totalChapters: { type: Number, default: 0 },
-  voteCount: { type: Number, default: 0 },
-  cover: { type: String },
-  genre: { type: String, required: true },
-  visibility: { type: String, required: true },
-  chapters: [
-    { _id: { type: String, ref: "Chapter", default: () => nanoid() } },
-  ],
-  author: {
-    username: { type: String, required: true },
-    userId: { type: String, required: true },
-  },
+    _id: { type: String, default: () => nanoid() },
+    summary: { type: String, required: true },
+    storyName: { type: String, required: true },
+    tags: [{ type: String }],
+    totalChapters: { type: Number, default: 0 },
+    voteCount: { type: Number, default: 0 },
+    cover: { type: String },
+    genre: { type: String, required: true },
+    visibility: { type: String, required: true, enum: ["public", "private"] },
+    chapters: [
+        { _id: { type: String, ref: "Chapter", default: () => nanoid() } },
+    ],
+    author: {
+        username: { type: String, required: true },
+        userId: { type: String, required: true },
+    },
 });
 module.exports = mongoose.model("Story", storySchema);
