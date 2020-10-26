b0e60a0773debbfa70781a59e9c8a1c6da0735fa
Sphoorthi Gaddam <sphoorthigaddam@gmail.com>
2020-10-26 07:02:08 -0500

    implementing get story data and incresing vote counter

--- routes/story.js
+++ routes/story.js
@@ -1,39 +1,61 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
+const Story = require('../models/Story');
 
 // @desc Story summary
 // @route GET /story/:storyId
 // @access Public
 router.get("/:storyId", (req, res) => {
-  res.send("fetch story details with that params.storyId");
+    const { storyId } = req.params.storyId;
+    try {
+        Story.findOne({ _id: storyId }, (err, story) => {
+            if (!story) throw "Invalid Story ID";
+        }).lean();
+        res.send("fetch story details with that params.storyId");
+    } catch (err) {
+        console.log(err);
+    }
 });
 
 // @desc Story Vote update
 // @route PUT /story/:storyId/vote
 // @access Private
 router.put("/:storyId/vote", (req, res) => {
-  res.send("increase the story vote counter");
+    const { storyId } = req.params.storyId;
+
+    try {
+        Story.findOne({ _id: storyId }, (err, story) => {
+            if (!story) throw "Invalid Story ID";
+        });
+
+        var voteCount = story.voteCount + 1;
+        var newvalues = { $set: { voteCount: voteCount } };
+        Story.updateOne({ ...req.body, storyId }, { _id: storyId }, newvalues);
+        res.send("increase the story vote counter");
+    } catch (err) {
+        console.log(err);
+    }
 });
 
 // @desc Chapter of the Story
 // @route GET /story/:storyId/chapter/:chapterId
 // @access Public
-router.get("/:storyId/chapter/:chapterId", (req, res) => {
-  res.send("fetch story details with that id");
+router.get('/:storyId/chapter/:chapterId', (req, res) => {
+	res.send('fetch story details with that id');
 });
 
 // @desc Add comment on a chapter
 // @route PUT /story/:storyId/chapter/:chapterId/comment
 // @access Private
-router.put("/:storyId/chapter/:chapterId/comment", (req, res) => {
-  res.send("update the comment data in chapter model of the story");
+router.put('/:storyId/chapter/:chapterId/comment', (req, res) => {
+	res.send('update the comment data in chapter model of the story');
 });
 
 // @desc Delete comment on a chapter
 // @route PUT /story/:storyId/chapter/:chapterId/comment/:commentId
 // @access Private
-router.delete("/:storyId/chapter/:chapterId/comment/:commentId", (req, res) => {
-  res.send("delete the comment data in chapter model of the story");
+router.delete('/:storyId/chapter/:chapterId/comment/:commentId', (req, res) => {
+	res.send('delete the comment data in chapter model of the story');
 });
 
 module.exports = router;
