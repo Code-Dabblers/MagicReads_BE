53d64fcc2a44c47e47a8222985099ec3ff4e6961
fabcodingzest <fabrizvi786@gmail.com>
2020-10-29 12:22:46 +0530

    added a get route for chapter details

--- routes/edit.js
+++ routes/edit.js
@@ -4,7 +4,7 @@ const router = express.Router();
 // @desc Edit story details
 // @route GET /edit/story/:storyID/details
 // @access Private
-router.put("/story/:storyID/details ", (req, res) => {
+router.get("/story/:storyID/details ", (req, res) => {
     res.send("get the story data");
 });
 
@@ -13,6 +13,13 @@ router.put("/story/:storyID/details ", (req, res) => {
 // @access Private
 router.put("/story/:storyID/details ", (req, res) => {
     res.send("its edit time, update the story data");
+});
+
+// @desc Edit chapter of a story
+// @route Get /edit/story/:storyID/details
+// @access Private
+router.get("/story/:storyID/chapter/:chapterId ", (req, res) => {
+    res.send("its edit time, update the chapter data");
 });
 
 // @desc Edit chapter of a story
