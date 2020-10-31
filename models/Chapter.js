04c88665eb45d2c10ab7424a276b14b9e42bd4f9
fabcodingzest <fabrizvi786@gmail.com>
2020-10-31 23:39:33 +0530

    Updated Chapter Model

--- models/Chapter.js
+++ models/Chapter.js
@@ -3,24 +3,16 @@ let { customAlphabet } = require("nanoid");
 const nanoid = customAlphabet("1234567890abcdef", 10);
 
 const chapterSchema = new mongoose.Schema({
-  _id: { type: String, required: true, default: () => nanoid() },
-  storyId: { type: String, required: true },
-  chapterTitle: { type: String, required: true },
-  chapterCover: { type: String },
-  chapter: { type: String, required: true },
-  comments: [
-    {
-      _id: { type: String, default: () => nanoid() },
-      storyId: { type: String, required: true },
-      username: { type: String, required: true },
-      userId: { type: String, required: true },
-      comment: { type: String, required: true },
+    _id: { type: String, required: true, default: () => nanoid() },
+    storyId: { type: String, required: true },
+    chapterTitle: { type: String, required: true },
+    chapterCover: { type: String },
+    chapter: { type: String, required: true },
+    comments: [{ type: String, ref: "Comment" }],
+    author: {
+        username: { type: String, required: true },
+        userId: { type: String, required: true },
     },
-  ],
-  author: {
-    username: { type: String, required: true },
-    userId: { type: String, required: true },
-  },
 });
 
 module.exports = mongoose.model("Chapter", chapterSchema);
