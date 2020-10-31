d5093aca87dac1b3e7625c5ac0cbbf5067a5544e
fabcodingzest <fabrizvi786@gmail.com>
2020-10-31 07:53:22 +0530

    corrected get & update Reading List- issue #18 #16

--- routes/user.js
+++ routes/user.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcrypt");
 const passport = require("passport");
+const Story = require("../models/Story");
 const BCRYPT_SALT_ROUNDS = 12;
 const User = require("../models/User");
 
@@ -24,7 +25,6 @@ router.post("/register", (req, res) => {
         .then((user) => {
             if (user) {
                 console.log("email already taken");
-                res.setHeader("Content-Type", "application/json");
                 res.status(201).send({
                     auth: false,
                     data: "user exists already",
@@ -45,7 +45,6 @@ router.post("/register", (req, res) => {
                                     { _id: user._id },
                                     process.env.JWT_SECRET
                                 );
-                                console.log("user created");
                                 res.status(200).send({
                                     auth: true,
                                     token: token,
@@ -108,54 +107,60 @@ router.post("/login", (req, res) => {
 });
 
 // @desc Add story to User's Reading List
-// @route POST /user/:storyId/readingList
+// @route PUT /user/:storyId/readingList
 // @access Private
-router.post("/:storyId/readingList", 
-passport.authenticate("jwt", { session: false }),
-async(req, res) => {
+router.put(
+    "/:storyId/readingList",
+    passport.authenticate("jwt", { session: false }),
+    async (req, res) => {
         try {
+            const { storyId } = req.params;
             await Story.findOne({ _id: storyId }, async function (err, story) {
                 if (err) res.status(404).send({ message: err.message });
                 await User.updateOne(
                     { _id: req.user._id },
-                    { $push: { readingList: { _id: story._id } } }
+                    { $addToSet: { "readingList.list": story._id } },
+                    (err, user) => {
+                        if (!user) throw err;
+                        res.status(200).send({
+                            message: "Story Added to the reading list",
+                            storyId: story._id,
+                        });
+                    }
                 );
-                res.status(200).send({
-                    message: "Story Added to the reading list",
-                    storyId: story._id,
-                });
             });
         } catch (err) {
-            res.status(500).send({ message: "Internal Server Error" });
+            res.status(500).send({
+                message: "Internal Server Error",
+                error: err.message,
+            });
+        }
     }
-   // res.send("Add the story to user reading list");
-});
+);
 
 // @desc User Reading List
 // @route GET /user/readingList
 // @access Private
-router.get("/readingList", 
-passport.authenticate("jwt", { session: false }),
-async(req, res) => {
-    try {
-        const stories = await Story.find({
-          user: req.params._id,
-          visibility: 'public',
-        })
-          .populate('user')
-          .lean()
-    
-          res.status(200).send({
-            message: "Reading list of user",
-            storyId: {stories}
-        });
-      } catch (err) {
-        res.status(500).send({ message: "Internal Server Error" });
-      }
-    // res.send(
-    //     "User will have an array of id's of stories in their readingList array in db, fetch them and send them as response"
-    // );
-});
+router.get(
+    "/readingList",
+    passport.authenticate("jwt", { session: false }),
+    async (req, res) => {
+        try {
+            const userData = await User.findOne({ _id: req.user._id })
+                .populate({ path: "readingList.list", model: "Story" })
+                .lean();
+            res.status(200).send({
+                message: "Reading List Found",
+                readingList: userData.readingList,
+            });
+        } catch (err) {
+            res.status(500).send({
+                message: "Internal Server Error",
+                error: err.message,
+            });
+        }
+    }
+);
 
 // @desc Add story to User's Library
 // @route POST /user/:storyId/readingList
@@ -176,47 +181,57 @@ router.get("/library", (req, res) => {
 // @desc User Settings
 // @route GET /user/settings
 // @access Private
-router.get("/settings", 
+router.get(
+    "/settings",
     passport.authenticate("jwt", { session: false }),
     async (req, res) => {
-    try {
-        await User.findOne({_id: req.user._id}, (err, user) => {
-            console.log(user)
-            const data = {
-                username: user.username,
-                email: user.email,
-                firstname: user.firstName,
-                lastname: user.lastName
-            }
-            res.status(200).send({ userData: data })
-        }).lean()
-    } catch(err) {
-        res.status(500).send({ 
-            message: "Internal Server Error",
-            error: err.message })
+        try {
+            await User.findOne({ _id: req.user._id }, (err, user) => {
+                const data = {
+                    username: user.username,
+                    firstname: user.firstName,
+                    lastname: user.lastName,
+                };
+                res.status(200).send({
+                    userData: data,
+                    message: "Current User Settings",
+                });
+            });
+        } catch (err) {
+            res.status(500).send({
+                message: "Internal Server Error",
+                error: err.message,
+            });
+        }
     }
-});
+);
 
 // @desc User Settings
 // @route PUT /user/settings
 // @access Private
-router.put("/settings", 
+router.put(
+    "/settings",
     passport.authenticate("jwt", { session: false }),
     async (req, res) => {
-
-    try {
-        const userDetails = await User.findOne({_id: req.user._id}, async(err, user)=>{
-            if(!user) res.status(404).send({message: "User not found"})
-            await User.updateOne({_id: user._id}, {$set: req.body})
-            res.status(200).send({message: "Updated user settings"})
-        })
-    } catch (err) {
-        res.status(500).send({ 
-            message: "Internal Server Error",
-            error: err.message })
+        try {
+            const { username, firstName, lastName } = req.body;
+            const dataObj = {};
+            if (username) dataObj.username = username;
+            if (firstName) dataObj.firstName = firstName;
+            if (lastName) dataObj.lastName = lastName;
+            await User.findOne({ _id: req.user._id }, async (err, user) => {
+                if (!user) res.status(404).send({ message: "User not found" });
+                await User.updateOne({ _id: user._id }, { $set: dataObj });
+                res.status(200).send({ message: "Updated user settings" });
+            });
+        } catch (err) {
+            res.status(500).send({
+                message: "Internal Server Error",
+                error: err.message,
+            });
+        }
     }
-    
-});
+);
 
 // @desc Logout page
 // @route GET /user/logout
