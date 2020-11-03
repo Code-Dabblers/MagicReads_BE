bf7609eee97bd9dd68652ddd8b2f31684ea05d5a
fabcodingzest <fabrizvi786@gmail.com>
2020-11-04 00:22:58 +0530

    Added deployed server

--- server.js
+++ server.js
@@ -37,6 +37,10 @@ const swaggerOptions = {
         },
         servers: [
             {
+                url: "https://magicreads.herokuapp.com",
+                description: "hosted server",
+            },
+            {
                 url: "http://localhost:3000",
                 description: "Development Server",
             },
@@ -72,7 +76,11 @@ app.use(express.json());
 // Method override used for PUT or DELETE requests
 app.use(
     methodOverride(function (req, res) {
-        if (req.body && typeof req.body === "object" && "_method" in req.body) {
+        if (
+            req.body &&
+            typeof req.body === "object" &&
+            "_method" in req.body
+        ) {
             let method = req.body._method;
             delete req.body._method;
             return method;
